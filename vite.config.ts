c636f6ba8256100ca5e7fb10a1f8fb9c70eba5f0
aemsyafii <aemsyafii@gmail.com>
2025-07-31 20:06:05 +0700

    perbaikan tulisan dikit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Search-Suggestion-Generator/',
   plugins: [react()],
   server: {
     port: 3000
